24f5db203b208ad7c5811b6763ace51c7a2d9c7e
VladSmirN <yavlads2014@yandex.ru>
2023-07-14 15:33:56 +0500

    change train parameters

--- train.py
+++ train.py
@@ -5,6 +5,6 @@ trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="cabile/")
 
-trainer.setTrainConfig(object_names_array=["cabile"],  train_from_pretrained_model="yolov3.pt")
+trainer.setTrainConfig(object_names_array=["cabile"], num_experiments=300, batch_size=16,  train_from_pretrained_model="yolov3.pt")
 
 trainer.trainModel()
